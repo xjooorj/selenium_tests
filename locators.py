6b86dc81352cf50a3eb0e8a6225742bde00c0050
xjooorj <notyourjorj@gmail.com>
2026-06-27 21:42:20 +0500

    Xpath and CSSSelector

--- locators.py
+++ locators.py
@@ -9,3 +9,12 @@ driver.maximize_window()
 driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("12345")
 driver.find_element(By.ID, "exampleCheck1").click()
+driver.find_element(By.CSS_SELECTOR, "input[name = 'name']").send_keys("george")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+driver.find_element(By.XPATH, "//input[@type = 'submit']").click()
+message = driver.find_element(By.CLASS_NAME, "alert").text
+print(message)
+assert "Success" in message
+
+driver.find_element(By.XPATH, "(//input[@type = 'text'])[3]").send_keys("hello")
+driver.find_element(By.XPATH, "(//input[@type = 'text'])[3]").clear()
