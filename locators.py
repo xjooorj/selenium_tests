5d27fdfb3c6dd0afb4326d1ae539e3b269e007c4
xjooorj <notyourjorj@gmail.com>
2026-06-28 18:48:07 +0500

    minor additions

--- locators.py
+++ locators.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Firefox()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
@@ -11,6 +12,10 @@ driver.find_element(By.ID, "exampleInputPassword1").send_keys("12345")
 driver.find_element(By.ID, "exampleCheck1").click()
 driver.find_element(By.CSS_SELECTOR, "input[name = 'name']").send_keys("george")
 driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+#Static Dropdown
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+#dropdown.select_by_index(1)
+dropdown.select_by_visible_text("Female")
 driver.find_element(By.XPATH, "//input[@type = 'submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert").text
 print(message)
