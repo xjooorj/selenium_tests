7511755507f4de32bbdd53ddd5cf580b813e5855
xjooorj <notyourjorj@gmail.com>
2026-07-08 11:56:18 +0500

    .

---
+++ waitDemo.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+driver = webdriver.Firefox()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/#top")
